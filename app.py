eb91f9a7a81c87c40bc12a4d8fde12a45c7bb37e
sandcha <sandcha@users.noreply.github.com>
2020-01-25 12:32:36 +0100

    Séparation api web et nombre communes éligibles à la dsr péréquation

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from back.dotation_solidarite_rurale import dotation_solidarite_rurale
 
-import pandas
-import os
 import json
 from flask import Flask, render_template
 from flask_cors import CORS
+
+from back.dotation_solidarite_rurale import eligible_dsr
+
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def hello_world():
-    print(os.listdir())
+    # source : https://leafletjs.com/examples/quick-start/
     return render_template('index.html')
+
 
 @app.route('/communes_polygons')
 def communes_polygons():
@@ -32,29 +33,6 @@ def communes_eligibles():
     }
 
 
-# communes_criteres_repartition_2019 = pandas.read_csv(
-#     './back/inputs/2019-communes-criteres-repartition.csv',
-#     decimal=",")
-# # print(communes_criteres_repartition_2019.keys())
-# 
-# assert len(communes_criteres_repartition_2019["Informations générales - Nom de la commune"]) == 35056
-# 
-# nombre_habitants = communes_criteres_repartition_2019[
-#     "Informations générales - Population DGF Année N'"
-#     ]
-# pfi_habitant = communes_criteres_repartition_2019[
-#     "Potentiel fiscal et financier des communes - Potentiel financier par habitant"
-#     ]
-# print(pfi_habitant)
-# print(dotation_solidarite_rurale(nombre_habitants, pfi_habitant))
-
-
-# from entites import Commune
-#
-# c = Commune('c', 1234, 1234, 1234)
-# dotation_solidarite_rurale = c.dotation_solidarite_rurale(1500, 1500)
-# print("éligible ?", dotation_solidarite_rurale)
-
-
 if __name__ == '__main__':
    app.run(debug=True)
+   
